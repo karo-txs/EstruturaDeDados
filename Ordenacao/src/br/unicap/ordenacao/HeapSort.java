0f1c9499e434ea543c835f179abd6575f3422383
luizfernando-o <luiz.2019230186@unicap.br>
2021-05-11 12:52:56 -0300

    heap sort

--- Ordenacao/src/br/unicap/ordenacao/HeapSort.java
+++ Ordenacao/src/br/unicap/ordenacao/HeapSort.java
@@ -1,11 +1,36 @@
 package br.unicap.ordenacao;
 
-  
-public class HeapSort  implements Sort{
+public class HeapSort implements Sort {
 
     @Override
     public int[] sort(int[] nos) {
+
+        int i, temp;
+        for (i = nos.length / 2 - 1; i >= 0; i--) {
+            heapify(nos, nos.length, i);
+        }
+        for (i = nos.length - 1; i >= 0; i--) {
+            temp = nos[0];
+            nos[0] = nos[i];
+            nos[i] = temp;
+            heapify(nos, i, 0);
+        }
         return nos;
     }
-    
+
+    void heapify(int arr[], int size, int i) {
+        int temp, largest = i;
+        int left = 2 * i + 1;
+        int right = left + 1;
+
+        largest = left < size && arr[left] > arr[largest] ? left : largest;
+        largest = right < size && arr[right] > arr[largest] ? right : largest;
+
+        if (largest != i) {
+            temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            heapify(arr, size, largest);
+        }
+    }
 }
