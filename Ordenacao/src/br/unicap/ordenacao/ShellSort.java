d1cd2056ff3a4b31bcc8a53cc68db19b6cf3ddd4
Karol <karolayne.2019230005@unicap.br>
2021-05-11 11:07:16 -0300

    shellsort

--- Ordenacao/src/br/unicap/ordenacao/ShellSort.java
+++ Ordenacao/src/br/unicap/ordenacao/ShellSort.java
@@ -4,7 +4,16 @@ public class ShellSort implements Sort {
 
     @Override
     public int[] sort(int[] nos) {
+        int j, atual;
+        for(int gap=nos.length/2;gap>0;gap/=2)
+            for (int i = gap; i < nos.length; i += 1) {
+                j = i;
+                atual = nos[i];
+                for (; j >= gap && nos[j - gap] > atual; j -= gap) {
+                    nos[j] = nos[j - gap];
+                }
+                nos[j] = atual;
+            }
         return nos;
     }
-
 }
